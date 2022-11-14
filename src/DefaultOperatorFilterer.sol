c87d16a48c1b10caf60d6ea5d8c3458f3309ac90
pacmanblur <100058240+pacmanblur@users.noreply.github.com>
2022-11-13 23:23:01 -0800

    Update DefaultOperatorFilterer.sol

--- src/DefaultOperatorFilterer.sol
+++ src/DefaultOperatorFilterer.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {OperatorFilterer} from "./OperatorFilterer.sol";
 
 abstract contract DefaultOperatorFilterer is OperatorFilterer {
-    address constant DEFAULT_SUBSCRIPTION = address(0x3cc6CddA760b79bAfa08dF41ECFA224f810dCeB6);
+    address constant DEFAULT_SUBSCRIPTION = address(0xFD7bfa171B5b81b79C245456E986db2f32fBFaDb);
 
     constructor() OperatorFilterer(DEFAULT_SUBSCRIPTION, true) {}
 }
