965060272dac888f8f3a9cff291b8af76082cba9
pacmanblur <100058240+pacmanblur@users.noreply.github.com>
2023-02-01 19:20:09 -0800

    Update DefaultOperatorFilterer.sol
    Switch to new filter address that removes X2Y2 and adds Seaport

--- src/DefaultOperatorFilterer.sol
+++ src/DefaultOperatorFilterer.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {OperatorFilterer} from "./OperatorFilterer.sol";
 
 abstract contract DefaultOperatorFilterer is OperatorFilterer {
-    address constant DEFAULT_SUBSCRIPTION = address(0xFD7bfa171B5b81b79C245456E986db2f32fBFaDb);
+    address constant DEFAULT_SUBSCRIPTION = address(0x9dC5EE2D52d014f8b81D662FA8f4CA525F27cD6b);
 
     constructor() OperatorFilterer(DEFAULT_SUBSCRIPTION, true) {}
 }
